a0ea3f34d63a012d48a16d6eb642b0414232bacf
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-12-05 20:44:15 +0100

    Install API Platform

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,7 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
