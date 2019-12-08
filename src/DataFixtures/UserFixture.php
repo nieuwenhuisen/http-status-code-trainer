92e5e790574195d4025f3b57b8d67131f6565907
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-12-08 19:13:46 +0100

    Add JWT authentication

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -11,6 +11,8 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
+    public const DEFAULT_PASSWORD = 'Test1234';
+
     private $faker;
     private $userPasswordEncoder;
 
@@ -22,16 +24,28 @@ class UserFixture extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        for ($i = 0; $i < 10; $i++) {
-            $user = new User();
-            $user->setEmail($this->faker->email);
+        // Create admin
+        $admin = $this->createUser('admin@admin.com', self::DEFAULT_PASSWORD, ['ROLE_ADMIN']);
+        $manager->persist($admin);
+        $this->addReference('admin', $admin);
 
-            $password = $this->userPasswordEncoder->encodePassword($user, 'Test1234');
-            $user->setPassword($password);
-
+        // Create 10 random users
+        for ($i = 1; $i <= 10; $i++) {
+            $user = $this->createUser($this->faker->email, self::DEFAULT_PASSWORD);
             $manager->persist($user);
+            $this->addReference('user_'.$i, $admin);
         }
 
         $manager->flush();
+    }
+
+    private function createUser(string $email, string $plainPassword, array $roles = []): User
+    {
+        $user = new User($email, $roles);
+
+        $password = $this->userPasswordEncoder->encodePassword($user, $plainPassword);
+        $user->changePassword($password);
+
+        return $user;
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,16 +4,23 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource(
+ *     collectionOperations={
+ *      "get"={
+ *          "access_control"="is_granted('ROLE_ADMIN')"
+ *      },
+ *      "post"
+ *     },
  *     normalizationContext={"groups"={"user:read"}}
  * )
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User implements UserInterface
+class User implements UserInterface, JWTUserInterface
 {
     /**
      * @ORM\Id()
@@ -31,7 +38,7 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="json")
      */
-    private $roles = [];
+    private $roles;
 
     /**
      * @var string The hashed password
@@ -39,81 +46,60 @@ class User implements UserInterface
      */
     private $password;
 
-    public function getId(): ?int
+    public function __construct(string $email, array $roles = [])
+    {
+        $this->email = $email;
+        $this->roles = $roles;
+    }
+
+    public function getId(): int
     {
         return $this->id;
     }
 
-    public function getEmail(): ?string
+    public function getEmail(): string
     {
         return $this->email;
     }
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
     public function getUsername(): string
     {
-        return (string) $this->email;
+        return $this->email;
     }
 
-    /**
-     * @see UserInterface
-     */
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * @see UserInterface
-     */
     public function getPassword(): string
     {
-        return (string) $this->password;
+        return $this->password;
     }
 
-    public function setPassword(string $password): self
+    public function changePassword(string $password): self
     {
         $this->password = $password;
 
         return $this;
     }
 
-    /**
-     * @see UserInterface
-     */
-    public function getSalt()
+    public function getSalt(): void
     {
-        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    public function eraseCredentials(): void
+    {
     }
 
     /**
-     * @see UserInterface
+     * @inheritDoc
      */
-    public function eraseCredentials()
+    public static function createFromPayload($username, array $payload)
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        new self($username, $payload['roles']);
     }
 }
