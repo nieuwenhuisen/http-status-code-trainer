f4e21497adfcb1a387b62cc647e7e2d2838e21d0
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-12-15 11:44:06 +0100

    Add user resource tests: get list, update, get detail, delete

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,46 +5,39 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
     public const DEFAULT_PASSWORD = 'Test1234';
 
-    private $faker;
+    /**
+     * List with dummy users.
+     * email, roles, plain password
+     */
+    private const USERS = [
+        'admin' => ['admin@admin.com', self::DEFAULT_PASSWORD, ['ROLE_ADMIN']],
+        'user_1' => ['user1@user.com', self::DEFAULT_PASSWORD, ['ROLE_USER']],
+        'user_2' => ['user2@user.com', self::DEFAULT_PASSWORD, ['ROLE_USER']],
+        'user_3' => ['user3@user.com', self::DEFAULT_PASSWORD, ['ROLE_USER']],
+    ];
+
     private $userPasswordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
     {
-        $this->faker = Factory::create();
         $this->userPasswordEncoder = $userPasswordEncoder;
     }
 
     public function load(ObjectManager $manager): void
     {
-        // Create admin
-        $admin = $this->createUser('admin@admin.com', self::DEFAULT_PASSWORD, ['ROLE_ADMIN']);
-        $manager->persist($admin);
-        $this->addReference('admin', $admin);
-
-        // Create 3 random users
-        for ($i = 1; $i <= 3; $i++) {
-            $user = $this->createUser($this->faker->email, self::DEFAULT_PASSWORD);
+        foreach (self::USERS as $index => [$email, $password, $roles]) {
+            $user = new User($email, $roles);
+            $user->setPassword($this->userPasswordEncoder->encodePassword($user, $password));
             $manager->persist($user);
-            $this->addReference('user_'.$i, $admin);
+            $this->addReference($index, $user);
         }
 
         $manager->flush();
-    }
-
-    private function createUser(string $email, string $plainPassword, array $roles = []): User
-    {
-        $user = new User($email, $roles);
-
-        $password = $this->userPasswordEncoder->encodePassword($user, $plainPassword);
-        $user->setPassword($password);
-
-        return $user;
     }
 }
--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,18 +2,39 @@
 
 namespace App\Tests\Functional;
 
+use App\DataFixtures\StatusCodeFixture;
 use App\DataFixtures\UserFixture;
+use App\Entity\User;
 use App\Test\ApiTestCase;
 
 class UserResourceTest extends ApiTestCase
 {
-    public function testCreateUser(): void
+    public function testGetUsers(): void
     {
         $this->loadFixtures([UserFixture::class]);
 
         $client = static::createAuthenticatedClient('admin@admin.com');
+        $response = $client->request('GET', '/users');
 
-        $response = $client->request('POST', '/users', ['json' => [
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+
+        $this->assertJsonContains([
+            '@context' => '/contexts/User',
+            '@id' => '/users',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 4
+        ]);
+
+        $this->assertCount(4, $response->toArray()['hydra:member']);
+    }
+
+    public function testCreateUser(): void
+    {
+        $this->loadFixtures([UserFixture::class]);
+        $client = static::createAuthenticatedClient('admin@admin.com');
+
+        $client->request('POST', '/users', ['json' => [
             'email' => 'john@gmail.com',
             'password' => 'Test123',
         ]]);
@@ -27,5 +48,56 @@ class UserResourceTest extends ApiTestCase
             'email' => 'john@gmail.com',
             'id' => 5,
         ]);
+    }
+
+    public function testGetUserDetail(): void
+    {
+        $this->loadFixtures([UserFixture::class, StatusCodeFixture::class]);
+        $client = static::createAuthenticatedClient('admin@admin.com');
+
+        $iri = static::findIriBy(User::class, ['email' => 'user1@user.com']);
+
+        $client->request('GET', $iri);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            '@context' => '/contexts/User',
+            '@id' => $iri,
+            '@type' => 'User',
+            'email' => 'user1@user.com'
+        ]);
+    }
+
+    public function testUpdateUser(): void
+    {
+        $this->loadFixtures([UserFixture::class]);
+        $client = static::createAuthenticatedClient('admin@admin.com');
+
+        $iri = static::findIriBy(User::class, ['email' => 'user1@user.com']);
+
+        $client->request('PUT', $iri, ['json' => [
+            'email' => 'user1@example.com',
+        ]]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            '@id' => $iri,
+            'email' => 'user1@example.com',
+        ]);
+    }
+
+    public function testDeleteUser(): void
+    {
+        $this->loadFixtures([UserFixture::class]);
+        $client = static::createAuthenticatedClient('admin@admin.com');
+
+        $iri = static::findIriBy(User::class, ['email' => 'user1@user.com']);
+
+        $client->request('DELETE', $iri);
+        $this->assertResponseStatusCodeSame(204);
+
+        $this->assertNull(
+            $this->getRepository(User::class)->findOneBy(['email' => 'user1@user.com'])
+        );
     }
 }
